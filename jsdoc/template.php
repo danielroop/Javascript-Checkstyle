270ce5899d8bb8a063b754f71d5ebd52aa16d826
pottedmeat <pottedmeat@560b804f-0ae3-0310-86f3-f6aa0a117693>
2008-02-14 21:03:39 +0000

    Refs #5602. Further caching updates for cron

--- jsdoc/template.php
+++ jsdoc/template.php
@@ -7,45 +7,7 @@ function phptemplate_jsdoc_object_form($form) {
 
 // Use these functions to create the equivalent tpl.php files
 
-function phptemplate_jsdoc_object_resources($node) {
-  return _phptemplate_callback('jsdoc_object_resources', array('node' => $node));
-}
-
-function phptemplate_jsdoc_object_type($node) {
-  return _phptemplate_callback('jsdoc_object_type', array('node' => $node));
-}
-
-/**
- * Theme the statement used to require this file
- */
-function phptemplate_jsdoc_object_require($node) {
-  return _phptemplate_callback('jsdoc_object_require', array('node' => $node));
-}
-
-function phptemplate_jsdoc_function($node) {
-  return _phptemplate_callback('jsdoc_function', array('node' => $node));
-}
-
-function phptemplate_jsdoc_object($node) {
-  return _phptemplate_callback('jsdoc_object', array('node' => $node));
-}
-
-function phptemplate_jsdoc_object_parent($node) {
-  return _phptemplate_callback('jsdoc_object_parent', array('node' => $node));
-}
-
-function phptemplate_jsdoc_class_list($nodes) {
-  return _phptemplate_callback('jsdoc_class_list', array('nodes' => $nodes));
-}
-
-function phptemplate_jsdoc_function_list($nodes) {
-  return _phptemplate_callback('jsdoc_function_list', array('nodes' => $nodes));
-}
-
-function phptemplate_jsdoc_field_list($nodes) {
-  return _phptemplate_callback('jsdoc_field_list', array('nodes' => $nodes));
-}
-
-function phptemplate_jsdoc_any_list($nodes) {
-  return _phptemplate_callback('jsdoc_any_list', array('nodes' => $nodes));
+// function theme_jsdoc_object_children($children) {
+function phptemplate_jsdoc_object_children($children) {
+  return _phptemplate_callback('jsdoc_object_children', array('children' => $children));
 }
